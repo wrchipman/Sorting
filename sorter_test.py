88fa0cd022e8a2dbaf4115bfe00fcde2d58d12bc
William Chipman <wrchipman@gmail.com>
2017-05-28 08:37:20 -0400

    Added test

---
+++ sorter_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from quicksort import QuickSort
+
+test_list = [43, 32, 75, 34, 76, 355, 900, 23, 1, 56, 38, 9, 8, 7, 6, 5, 4, 99 ]
+
+QuickSort(test_list)
+
+print test_list
